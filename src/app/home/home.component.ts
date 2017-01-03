6bbeb401d109e770180084a6d165b761d80432a8
Xin J Zheng <Xin J Zheng@au.aap.ad.pwcinternal.com>
2017-01-03 20:13:03 +1100

    form data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@
  * Created by Xin J Zheng on 2/01/2017.
  */
 import {Component} from '@angular/core';
+import {Employee} from "../models/employee.model";
 
 @Component({
   selector: 'home',
@@ -9,4 +10,14 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages: string[] = ['English', 'Spanish', 'Other'];
+  model = new Employee('Darla', 'Smith', true, 'w2', 'English');
+
+  // firstNameToUpperCase(value: string): void {
+  //   if(value.length>0) {
+  //     this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+  //   } else {
+  //     this.model.firstName = value;
+  //   }
+  // }
 }
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Xin J Zheng on 3/01/2017.
+ */
+export class Employee {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public isFullTime: boolean,
+    public paymentType: string,
+    public primaryLanguage: string
+  ) {
+  }
+}
